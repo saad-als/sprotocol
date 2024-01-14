a3840d2e9f81feb5910ee7d7528fca689d4bbaef
SaadAlsayed <smqaa.github@gmail.com>
2024-01-14 21:36:23 +0300

    this file is for creating a secure encrypted code to be sent from sender to recevier

---
+++ src/backend/secureCodeGenerator.py
@@ -0,0 +1,13 @@
+class Generator:
+
+    # a def for creating the secure code by sender request
+    def createCode(self, user_name):
+        pass
+
+    # for checking if the secure code match the original sent code
+    def checkCode(self, secure_code_id):
+        pass
+
+    # this is for removing the secure code after it is used
+    def removeCode(self, secure_code_id):
+        pass
