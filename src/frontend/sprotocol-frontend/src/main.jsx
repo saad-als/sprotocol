d33e437dd9b7f67ca280a23f1074023214b2f619
SaadAlsayed <smqaa.github@gmail.com>
2024-01-15 14:39:06 +0300

    importing boostrap js

--- src/frontend/sprotocol-frontend/src/main.jsx
+++ src/frontend/sprotocol-frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
