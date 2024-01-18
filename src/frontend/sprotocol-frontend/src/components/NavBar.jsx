8bd258f345433436bb459063b41d42ee4c46e5d9
SaadAlsayed <smqaa.github@gmail.com>
2024-01-18 17:43:47 +0300

    adding a navbar with a logo and dropdown menu

---
+++ src/frontend/sprotocol-frontend/src/components/NavBar.jsx
@@ -0,0 +1,30 @@
+function NavBar() {
+    return (
+        <>
+
+            <nav className="navbar">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        <img src="./././assets/sprotocol-logos/sprotocol-logos_black.png" alt="Logo" width="100" height="100" className="d-inline-block align-text-top" />
+
+                    </a>
+
+                    <div className="d-flex me-4">
+                        <div className="dropstart">
+                            <button className="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">Dark theme</a></li>
+                                <li><a className="dropdown-item" href="#">Github</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            </nav>
+        </>
+    );
+}
+
+export default NavBar
