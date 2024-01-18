e138f64fbb3ec15ceb4138794d3fe015f377835d
SaadAlsayed <smqaa.github@gmail.com>
2024-01-18 14:39:36 +0300

    few fixes and including the LoginForm to the page

--- src/frontend/sprotocol-frontend/src/pages/HomePage.jsx
+++ src/frontend/sprotocol-frontend/src/pages/HomePage.jsx
@@ -1,7 +1,20 @@
+import LoginForm from '../components/LoginForm'
 function HomePage() {
 
     return (
         <>
+
+
+            <div className="container-md d-flex align-items-center justify-content-center w-50">
+
+                <div className="p-4 m-4 shadow rounded-2 ">
+
+                    <LoginForm />
+
+                </div>
+
+            </div>
+
 
         </>
     );
