d9daf4514a63edd8fb7598816f3113a3df886eee
SaadAlsayed <smqaa.github@gmail.com>
2024-01-18 22:10:38 +0300

    few fixes.

--- src/frontend/sprotocol-frontend/src/pages/HomePage.jsx
+++ src/frontend/sprotocol-frontend/src/pages/HomePage.jsx
@@ -1,19 +1,27 @@
+import NavBar from '../components/NavBar';
 import LoginForm from '../components/LoginForm'
+import '../styles/loginForm.css'
 function HomePage() {
 
     return (
         <>
 
 
+
+            <NavBar />
             <div className="container-md d-flex align-items-center justify-content-center w-50">
 
-                <div className="p-4 m-4 shadow rounded-2 ">
+                <div className="p-4 m-4 shadow rounded-2 login-form">
 
                     <LoginForm />
 
                 </div>
 
+
             </div>
+
+            <footer className=' w-100 p-2 text-center text-body-tertiary'>@ 2024 Copyright: Saad Alsayed</footer>
+
 
 
         </>
