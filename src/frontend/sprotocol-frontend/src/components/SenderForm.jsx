f350c2c8f530e9f0fb46fb2e8bd289e4813c5f91
SaadAlsayed <smqaa.github@gmail.com>
2024-01-19 14:16:16 +0300

    simple layout for the sender form, a user can enter the name and create a secure code to copy and send to recevier

--- src/frontend/sprotocol-frontend/src/components/SenderForm.jsx
+++ src/frontend/sprotocol-frontend/src/components/SenderForm.jsx
@@ -1,18 +1,34 @@
-function SecureForm() {
+function SenderForm() {
     return (
         <>
             <div className="sender-class ">
 
+                <form action="">
+                    <div className="form-floating mb-3">
+                        <input type="text" className="form-control" id="floatingInput" placeholder="..." />
+                        <label htmlFor="floatingInput">Your Name</label>
+
+                        <div className="d-inline-flex gap-3 m-2 form-floating">
+                            <div className="input-group mb-3">
+                                <span className="input-group-text"><button type="button" className="btn btn-outline-warning">generate code</button>
+                                </span>
+                                <div className="form-floating">
+                                    <input type="text" readOnly className="form-control-plaintext border border-primary" id="floatingPlaintextInput" placeholder="*****" />
+                                    {/* <label htmlFor="floatingInputGroup1">Secure Code</label> */}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
             </div>
 
-            <div className="recevier-class">
 
-            </div>
 
         </>
     );
 }
 
-export default SecureForm
+export default SenderForm
 
 
