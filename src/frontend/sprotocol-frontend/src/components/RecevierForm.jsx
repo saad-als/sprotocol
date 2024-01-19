c22b3168b65d96dbac32b48d507fab98636926e3
SaadAlsayed <smqaa.github@gmail.com>
2024-01-19 14:25:42 +0300

    added name, an input for secure code, and a join chat button

--- src/frontend/sprotocol-frontend/src/components/RecevierForm.jsx
+++ src/frontend/sprotocol-frontend/src/components/RecevierForm.jsx
@@ -3,6 +3,29 @@ function ReceiverForm() {
         <>
             <div className="recevier-class">
 
+                <form action="">
+                    <div className="form-floating mb-3">
+                        <input type="text" className="form-control" id="floatingInput" placeholder="..." />
+                        <label htmlFor="floatingInput">Your Name</label>
+
+                        <div className="d-inline-flex gap-3 m-2 form-floating">
+                            <div className="input-group mb-3">
+                                <span className="input-group-text">code</span>
+                                <div className="form-floating">
+                                    <input type="text" className="form-control-plaintext border border-primary" id="floatingPlaintextInput" placeholder="*****" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="form-floating mb-3">
+                            <button type="button" className="btn btn-warning mx-auto">Accept invite and Join Chat</button>
+
+                        </div>
+
+
+                    </div>
+                </form>
+
             </div>
         </>
     );
