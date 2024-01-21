7b484a9ed0e5d9c07b5a9d6737b9b494e48a5f58
SaadAlsayed <smqaa.github@gmail.com>
2024-01-21 19:12:23 +0300

    this what messageBubble component will look like

--- src/frontend/sprotocol-frontend/src/components/Chat/MessageBubble.jsx
+++ src/frontend/sprotocol-frontend/src/components/Chat/MessageBubble.jsx
@@ -1,6 +1,46 @@
+import '../../styles/message-style.css';
 function MessageBubble() {
     return (
         <>
+            <div className="card container-md border-warning dotted-border position-relative">
+                <div className="card-body">
+
+                    <div className="d-flex flex-row justify-content-start mb-4">
+                        <div className=" ms-3">
+                            <p className="small mb-0">
+                                Hello and thank you for visiting MDBootstrap. Please click the video
+                                below.
+                                <span className='position-absolute top-0 start-0 translate-middle badge bg-warning '>
+                                    sender
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
+
+            <div className="card container-md m-5 border-success dotted-border position-relative">
+                <div className="card-body">
+
+                    <div className="d-flex flex-row justify-content-end mb-4">
+                        <div className=" ms-3">
+                            <p className="small mb-0">
+                                Hello and thank you for visiting MDBootstrap. Please click the video
+                                below.
+                                <span className='position-absolute top-0 start-100 translate-middle badge bg-success '>
+                                    recevier
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
 
         </>
     );
