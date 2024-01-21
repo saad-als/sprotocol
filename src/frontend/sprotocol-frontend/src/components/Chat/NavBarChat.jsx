7fb24db4b3981b236c3bbb07fea8b6c657fbbb6b
SaadAlsayed <smqaa.github@gmail.com>
2024-01-21 21:08:37 +0300

    a navbar to chat page

---
+++ src/frontend/sprotocol-frontend/src/components/Chat/NavBarChat.jsx
@@ -0,0 +1,27 @@
+function NavBarChat() {
+    return (
+        <>
+
+            <nav className="navbar h-10">
+                <div className="container-fluid">
+                    <a className="navbar-brand d-flex align-items-center justify-content-center" href="#">
+                        <div className="sender ms-2">Sender </div>
+                        <div className="recevier ms-2">recevier</div>
+                    </a>
+
+                    <div className="d-flex me-4">
+                        <button type="button" className="btn btn-danger ">
+                            <span className="bedge small m-2"> <img src="../../../assets/stopwatch.svg" alt="stopwatch svg" /> </span>
+                            Stop
+                        </button>
+                    </div>
+                </div>
+
+            </nav>
+
+        </>
+    );
+}
+
+
+export default NavBarChat
