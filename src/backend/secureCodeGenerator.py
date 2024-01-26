44ece9e414197e0f0c5540054b13ae76e8245951
SaadAlsayed <smqaa.github@gmail.com>
2024-01-26 21:31:57 +0300

    trying to create a secure password with wekzeug.security

--- src/backend/secureCodeGenerator.py
+++ src/backend/secureCodeGenerator.py
@@ -1,13 +1,14 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
 class Generator:
 
+    # secure_password = here will be the database object
     # a def for creating the secure code by sender request
-    def createCode(self, user_name):
-        pass
+    def createCode(self, user_name, password):
+        self.secure_password = generate_password_hash(password)
+        # add secure code to db and username who generated it
 
     # for checking if the secure code match the original sent code
-    def checkCode(self, secure_code_id):
-        pass
-
-    # this is for removing the secure code after it is used
-    def removeCode(self, secure_code_id):
-        pass
+    def checkCode(self, password):
+        return check_password_hash(self.secure_password, password)
