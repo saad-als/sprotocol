2221add3ecd6f346144233cf391c5e08ff4c5dce
SaadAlsayed <smqaa.github@gmail.com>
2024-02-29 01:29:37 +0300

    trying to connect the sender and the recevier with the same secure hash code

--- src/backend/user.py
+++ src/backend/user.py
@@ -1,13 +1,14 @@
 from .database import User, db
+from .secureCodeGenerator import Generator
 
 
 class User:
 
     def __init__(self, user_name="saad", secure_code="No s_code") -> None:
         self.user_name = user_name
-        self.secure_code = secure_code
+        self.secure_code = Generator.createCode(secure_code)
 
-    def createUser(self):
+    def createUser(self, username):
         user = User(self.user_name, self.secure_code)
         db.session.add(user)
         db.session.commit()
