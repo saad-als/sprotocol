e05b0113c680fa5f3f97a968239e82f5d25c0bc6
SaadAlsayed <smqaa.github@gmail.com>
2024-03-10 00:58:27 +0300

    added createRecevier function to check if a sender with specefic hash code exist

--- src/backend/user.py
+++ src/backend/user.py
@@ -4,11 +4,19 @@ from .secureCodeGenerator import Generator
 
 class User:
 
-    def __init__(self, user_name="saad", secure_code="No s_code") -> None:
+    def __init__(self, user_name=None) -> None:
         self.user_name = user_name
-        self.secure_code = Generator.createCode(secure_code)
+        self.secure_code = Generator.createCode(self.user_name)
 
-    def createUser(self, username):
-        user = User(self.user_name, self.secure_code)
+    def createSender(self, username):
+        user = User(username)
         db.session.add(user)
         db.session.commit()
+
+    def createRecevier(self, username, hash_code):
+        is_sender_exist = db.session.execute(
+            db.select(User).where(User.secure_code == hash_code)).scalar()
+        if is_sender_exist:
+            user = User(username)
+            db.session.add(user)
+            db.session.commit()
