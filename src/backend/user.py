ba9421c0f1adccd1dbc74a0bb5ad75630e5ab745
SaadAlsayed <smqaa.github@gmail.com>
2024-05-07 00:44:11 +0300

    adding some comments and few fixis

--- src/backend/user.py
+++ src/backend/user.py
@@ -1,6 +1,6 @@
-from .database import db, User
+from database import db, User
 from sqlalchemy import select
-from .secureCodeGenerator import generate_password_hash, check_password_hash
+from secureCodeGenerator import generate_password_hash, check_password_hash
 
 
 class User:
@@ -15,19 +15,19 @@ class User:
     # create a user - Recevier
     def create_recevier(self, username, sender_hash_code):
         try:
+            # query a user based on id which is (1) and check if hashcodes matches
             is_sender_exist = db.session.execute(
                 select(User).where(User.secure_code == sender_hash_code).filter(User.id == 1)).scalar_one()
 
             is_same_code = check_password_hash(
                 sender_hash_code, is_sender_exist.secure_code)
 
+            # if sender user exits, create a second user receiver and commit it to db
             if is_same_code:
                 self.user = User(id=2, username=username,
                                  secure_code=sender_hash_code)
-
                 db.session.add(self.user)
-
                 db.session.commit()
 
-        except:
-            pass
+        except Exception as er:
+            print("error occured : ", er)
