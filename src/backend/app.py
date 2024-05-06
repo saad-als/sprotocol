effb05ee866a5c66efc979d812a278d40e1e3b1a
SaadAlsayed <smqaa.github@gmail.com>
2024-05-07 00:38:27 +0300

    multiple things happening here, enabling CORS, figuring out how to send data from frontend to backend, few fixis

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask import request
-from .database import db
-from .user import User
+from flask_cors import CORS
+from database import db
+from user import User
 app = Flask(__name__)
+cors = CORS()
 # testing only
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db_sprotocol.db"
 db.init_app(app)
-
+cors.init_app(app)
 # create database
 with app.app_context():
     db.create_all()
@@ -15,7 +17,8 @@ with app.app_context():
 @app.route("/", methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        pass
+        user_data = request.get_json()
+        print("data from frontend is here", user_data)
 
     return "hello Saad this is / route"
 
--- src/frontend/sprotocol-frontend/src/components/Home/SenderForm.jsx
+++ src/frontend/sprotocol-frontend/src/components/Home/SenderForm.jsx
@@ -1,6 +1,23 @@
 import { useState } from "react";
 function SenderForm() {
 
+    // sending the form to the backend
+    const createUser = async () => {
+        const username = document.getElementsByName("username")[0].value;
+        const data = await fetch("http://localhost:5000", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(username)
+        })
+
+        if (data.ok) {
+            console.log("its ok")
+        }
+
+    };
+
 
     const [showLogin, setLogin] = useState(false);
     // const [userName, setUserName] = useState("");
@@ -10,12 +27,11 @@ function SenderForm() {
             return (<>
                 <div className="d-inline-flex gap-3 m-2 form-floating">
                     <div className="input-group mb-3">
-
-
-                        <input className="form-control" type="text" value="" placeholder="copy the code " aria-label="readonly input example" readOnly />
-
+                        <label htmlFor="code_label" className="">******</label>
                     </div>
                 </div>
+
+                <p className="fs-6 fw-light">copy secure code and send it to the Recevier</p>
 
                 <div className="form-floating mb-3">
                     <button type="button" className="btn btn-success mx-auto">Join Chat</button>
@@ -29,10 +45,18 @@ function SenderForm() {
             return (
                 <>
                     <div className="d-inline-flex form-floating mb-3">
-                        <input type="text" className="form-control" id="floatingInput" placeholder="..." />
+                        <input type="text" className="form-control" id="floatingInput" placeholder="..." name="username" />
                         <label htmlFor="floatingInput">Your Name</label>
                         <span className="input-group-text">
-                            <button type="submit" onClick={() => { setLogin(true) }} className="btn btn-outline-warning" >generate code</button>
+                            <button type="submit" onClick={() => {
+
+                                createUser()
+
+                                setLogin(true)
+
+
+
+                            }} className="btn btn-outline-warning" >generate code</button>
                         </span>
 
 
