d8f52362802abd1b316ebf28b992e535e3163a7d
SaadAlsayed <smqaa.github@gmail.com>
2024-05-26 16:16:10 +0300

    fixing ids to be auto incremental and removing unique argument from username

--- src/backend/database.py
+++ src/backend/database.py
@@ -12,13 +12,13 @@ db = SQLAlchemy(model_class=Base)
 
 class User(db.Model):
     __tablename__ = "user"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(unique=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column()
     secure_code: Mapped[str] = mapped_column(unique=True)
 
 
 class Message(db.Model):
     __tablename__ = "message"
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     message_content: Mapped[str]
     messages_owner: Mapped[int] = mapped_column(ForeignKey("user.id"))
